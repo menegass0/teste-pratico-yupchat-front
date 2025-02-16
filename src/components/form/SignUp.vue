026e1a3cdd540ff2eace32f9142ef01faaad1824
menegass0 <victormenegasso@unisantos.br>
2025-02-16 01:51:42 -0300

    função de registrar usuario

--- src/components/form/SignUp.vue
+++ src/components/form/SignUp.vue
@@ -2,22 +2,35 @@
 import InputGroup from './InputGroup.vue'
 import FormCard from './FormCard.vue'
 import { RouterLink } from 'vue-router'
+
+const props = defineProps(['handleSubmit', 'errorMessage'])
 </script>
 
 <template>
-  <FormCard title="Cadastro" action="Cadastre-se">
+  <FormCard
+    title="Cadastro"
+    action="Cadastre-se"
+    :handleSubmit
+    :errorMessage
+    @dismissAlert="$emit('dismissAlert')"
+  >
     <template #form>
       <InputGroup class="bi-person-fill">
-        <input type="text" class="form-control" placeholder="Nome de Usuário" />
+        <input type="text" name="name" class="form-control" placeholder="Nome de Usuário" />
       </InputGroup>
       <InputGroup class="bi-envelope-fill">
-        <input type="email" class="form-control" placeholder="E-mail" />
+        <input type="email" name="email" class="form-control" placeholder="E-mail" />
       </InputGroup>
       <InputGroup class="bi-key-fill">
-        <input type="password" class="form-control" placeholder="Senha" />
+        <input type="password" name="password" class="form-control" placeholder="Senha" />
       </InputGroup>
       <InputGroup class="bi-key-fill">
-        <input type="password" class="form-control" placeholder="Confirmar Senha" />
+        <input
+          type="password"
+          name="c_password"
+          class="form-control"
+          placeholder="Confirmar Senha"
+        />
       </InputGroup>
     </template>
     <template #extra>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import SignUp from '@/components/form/SignUp.vue';
+import SignUp from '@/components/form/SignUp.vue'
+import { ref } from 'vue'
+
+const errorMessage = ref('')
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  const formData = new FormData(event.target)
+
+  fetch('http://127.0.0.1:8000/api/register', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        console.log(data.message)
+      } else {
+        errorMessage.value = data.message
+      }
+    })
+}
+
+const dismissAlert = () => {
+  console.log('qaaaaaaaaa')
+  errorMessage.value = ''
+}
 </script>
 
 <template>
-    <main>
-        <SignUp />
-    </main>
+  <main>
+    <SignUp :handleSubmit :errorMessage @dismissAlert="dismissAlert" />
+  </main>
 </template>
